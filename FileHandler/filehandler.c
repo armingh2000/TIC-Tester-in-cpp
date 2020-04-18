c4988f31eabe71b88487f69cd44f8e60ce90c5ee
Armin <armingh379@gmail.com>
2020-04-19 00:19:36 +0430

    ignored nul in .gitignore, read_input imped

--- FileHandler/filehandler.c
+++ FileHandler/filehandler.c
@@ -1,4 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <dirent.h>
+
+// reads input file and return a 
+char ** read_input(char * file_path, int * ln)
+{
+    FILE * input_file = fopen(file_path, "r");
+    if(input_file == NULL) { fprintf(stderr, "input_file %s, not found", file_path); exit(EXIT_FAILURE);}
+    char ** inp = malloc(sizeof(*inp));
+    char * line = NULL;
+    size_t sz;
+    int lines_number= 0;
+    while(getline(&line, &sz, input_file) >= 0)
+    {
+        lines_number++;
+        inp = realloc(inp, lines_number * sizeof(*inp));
+        inp[lines_number - 1] = line;
+        line = NULL;
+    }
+    free(line);
+    fclose(input_file);
+    return inp;
+}
 
 
