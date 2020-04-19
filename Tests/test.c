68e6d0fc886a37bccb9f3e1102c36f362c1f035d
Armin <armingh379@gmail.com>
2020-04-20 00:37:08 +0430

    read_input_test imped

--- Tests/test.c
+++ Tests/test.c
@@ -6,13 +6,14 @@ char ** read_input(char * file_path, int * ln);
 
 void read_input_test()
 {
-    char fp[] = { "\\tests_dir\\In_1.txt" }; 
-    int fln = 0;
+    char fp[] = { "./tests_dir/In_1.txt" }; 
+    int __attribute__((unused)) fln = 0;
     char ** test1 = read_input(fp, &fln);
     for(int i = 0; i < fln; i++)
     {
         printf("%s\n", test1[i]);
     }
+    printf("%d", fln);
 
 }
 
