51df6a69dad7a58146920b0909376d7003abce52
Armin <armingh379@gmail.com>
2020-04-19 23:41:49 +0430

    read_input_test imped

---
+++ Tests/test.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+char ** read_input(char * file_path, int * ln);
+
+void read_input_test()
+{
+    char fp[] = { "\\tests_dir\\In_1.txt" }; 
+    int fln = 0;
+    char ** test1 = read_input(fp, &fln);
+    for(int i = 0; i < fln; i++)
+    {
+        printf("%s\n", test1[i]);
+    }
+
+}
+
+int main()
+{
+    // FileHandler tests
+
+    // read_input
+    read_input_test();
+
+    return EXIT_SUCCESS;
+}
+
