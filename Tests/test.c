052eeb6bc139780a01fcf05324534f0aa9da4973
subroot <subroot@localhost.localdomain>
2020-04-29 01:01:36 -0400

    added filehandler.c->free_read_file_test, changed interface to 2 possibilities; 1- run all tests; 2- run selected tests;

--- Tests/test.c
+++ Tests/test.c
@@ -2,9 +2,9 @@
 #include <stdlib.h>
 #include <assert.h>
 #include <string.h>
+#include "filehandler.h"
+#include "test.h"
 
-
-char ** read_file(char * file_path, int * ln);
 
 void read_file_test()
 {
@@ -12,7 +12,7 @@ void read_file_test()
 
     // test 1
     char fp1[] = { "./tests_dir/In_1.txt" }; 
-    int fln1 __attribute__((unused)) = 0;
+    int fln1 = 0;
     char ** test1 = read_file(fp1, &fln1);
     assert(strcmp(test1[0], "4") == 0);
     assert(strcmp(test1[1], "0 0") == 0);
@@ -20,22 +20,70 @@ void read_file_test()
     assert(strcmp(test1[3], "1 0") == 0);
     assert(strcmp(test1[4], "1 1") == 0);
     assert(fln1 == 5);
+    free_read_file(test1, fln1);
 
     // test 2
     char fp2[] = { "./tests_dir/In_2.txt" }; 
-    int fln2 __attribute__((unused)) = 0;
+    int fln2 = 0;
     char ** test2 = read_file(fp2, &fln2);
     assert(test2 == NULL);
-    fprintf(stdout, "%s\n", "*test successfully finished*");
+    free_read_file(test2, fln2);
 
+    fprintf(stdout, "%s\n", "*test successfully finished*");
 }
 
-int main()
+void free_read_file_test()
 {
+    fprintf(stdout, "starting filehandler.c->read_file_test\n");
+
+    // test 1
+    char fp1[] = { "./tests_dir/In_1.txt" }; 
+    int fln1 = 0;
+    char ** test1 = read_file(fp1, &fln1);
+    free_read_file(test1, fln1);
+
+    fprintf(stdout, "%s\n", "*test successfully finished*");
+}
+
+int main(int argc, char ** argv)
+{
+    if(argc == 1)
+    {
     // FileHandler tests
-    // read_input
+    
+    // read_file
     read_file_test();
 
+    // free_read_file_test
+    free_read_file_test();
+
+    }
+    else
+    {
+        if(strcmp(argv[1], "focused") != 0)
+            fprintf(stderr, "Option invalid; Either leave it empty or write focused");
+
+        int func_nums = 2;
+        char * func_names[] = { "read_file", "free_read_file" };
+        void (*funcs[])() = { &read_file_test, &free_read_file_test };
+        char * line = NULL;
+        size_t sz;
+
+        for(int i = 0; i < func_nums; i++)
+        {
+            printf("Do you want to run '%s' test?", func_names[i]);
+            if(getline(&line, &sz, stdin) > 0)
+            {
+                if(strcmp(line, "\n") == 0)
+                {
+                    (*(funcs[i]))();
+                }
+                else
+                    continue;
+            }
+        }
+        free(line);
+    }
     return EXIT_SUCCESS;
 }
 
