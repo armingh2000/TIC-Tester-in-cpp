d7a15d5351e8e7ab503e9300ee293f48b2d85f78
subroot <subroot@localhost.localdomain>
2020-04-28 22:02:14 -0400

    fixed test.c->read_file_test made it with assertions

--- Tests/test.c
+++ Tests/test.c
@@ -1,28 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <assert.h>
+#include <string.h>
 
 
-char ** read_input(char * file_path, int * ln);
+char ** read_file(char * file_path, int * ln);
 
-void read_input_test()
+void read_file_test()
 {
+    fprintf(stdout, "starting filehandler.c->read_file_test\n");
     char fp[] = { "./tests_dir/In_1.txt" }; 
-    int __attribute__((unused)) fln = 0;
-    char ** test1 = read_input(fp, &fln);
-    for(int i = 0; i < fln; i++)
-    {
-        printf("%s\n", test1[i]);
-    }
-    printf("%d", fln);
+    int fln __attribute__((unused)) = 0;
+    char ** test1 = read_file(fp, &fln);
+    assert(strcmp(test1[0], "4") == 0);
+    assert(strcmp(test1[1], "0 0") == 0);
+    assert(strcmp(test1[2], "0 1") == 0);
+    assert(strcmp(test1[3], "1 0") == 0);
+    assert(strcmp(test1[4], "1 1") == 0);
+    assert(fln == 5);
+    fprintf(stdout, "%s\n", "*test successfully finished*");
 
 }
 
 int main()
 {
     // FileHandler tests
-
     // read_input
-    read_input_test();
+    read_file_test();
 
     return EXIT_SUCCESS;
 }
